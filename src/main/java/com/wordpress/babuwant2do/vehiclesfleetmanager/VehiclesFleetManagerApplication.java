7a077b890f79dda3f62c51c8dc67d2da8b645c9f
Mohammed Ali <mohammed.ali@stentle.com>
2022-02-04 23:08:50 +0100

    initial commit

---
+++ src/main/java/com/wordpress/babuwant2do/vehiclesfleetmanager/VehiclesFleetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.babuwant2do.vehiclesfleetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesFleetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesFleetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordpress/babuwant2do/vehiclesfleetmanager/VehiclesFleetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.babuwant2do.vehiclesfleetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesFleetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
